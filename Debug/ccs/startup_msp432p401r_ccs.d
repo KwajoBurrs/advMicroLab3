1939aceffafd4627fc2aea5122c7b9488126bc82
KwajoBurrs <86699656+KwajoBurrs@users.noreply.github.com>
2025-02-11 18:56:12 -0500

    Add files via upload

---
+++ Debug/ccs/startup_msp432p401r_ccs.d
@@ -0,0 +1,36 @@
+# FIXED
+
+ccs/startup_msp432p401r_ccs.obj: ../ccs/startup_msp432p401r_ccs.c
+ccs/startup_msp432p401r_ccs.obj: D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+ccs/startup_msp432p401r_ccs.obj: D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+ccs/startup_msp432p401r_ccs.obj: D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+ccs/startup_msp432p401r_ccs.obj: D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+ccs/startup_msp432p401r_ccs.obj: D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+ccs/startup_msp432p401r_ccs.obj: D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+ccs/startup_msp432p401r_ccs.obj: D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+ccs/startup_msp432p401r_ccs.obj: D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+ccs/startup_msp432p401r_ccs.obj: D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+ccs/startup_msp432p401r_ccs.obj: D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+
+../ccs/startup_msp432p401r_ccs.c:
+
+D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+D:/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
